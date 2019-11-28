6a7e8ec5a34cc2ab6d2b19467c96b8c41d63e652
Pinelab <pinelab@seznam.cz>
2019-11-28 18:02:20 +0100

    eeprom ready to safe data

--- PinTest_WiFi.ino
+++ PinTest_WiFi.ino
@@ -3,9 +3,17 @@
  *  The API is almost the same as with the WiFi Shield library,
  *  the most obvious difference being the different file you need to include:
  */
+#include "EEPROM.h"
 #include "WiFi.h"
 static const uint8_t LED_BUILTIN = 32;
 #define BUILTIN_LED  LED_BUILTIN // backward compatibility
+
+int TemperatureArray[8]={0,1,2,3,4,5,6,7};  // temporal temperature array
+int loopCounter = 0; // main loop counter
+
+int addr = 0;
+#define EEPROM_SIZE 8
+
 
 void setup()
 {
@@ -18,11 +26,22 @@ void setup()
     delay(100);
 
     Serial.println("Setup done");
+
+  if (!EEPROM.begin(EEPROM_SIZE))
+  {
+    Serial.println("failed to initialise EEPROM"); delay(1000000);
+  }
+  Serial.println(" bytes read from Flash . Values are:");
+  for (int i = 0; i < EEPROM_SIZE; i++)
+  {
+    Serial.print(byte(EEPROM.read(i))); Serial.print(" ");
+  } 
 }
 
 void loop()
 {
   int i = 0;
+  loopCounter++;  
     Serial.println("scan start");
 
     // WiFi.scanNetworks will return the number of networks found
@@ -41,6 +60,7 @@ void loop()
         Serial.print(n);
         Serial.println(" networks found");
         for (int i = 0; i < n; ++i) {
+          
             // Print SSID and RSSI for each network found
             Serial.print(i + 1);
             Serial.print(": ");
@@ -53,7 +73,27 @@ void loop()
         }
     }
     Serial.println("");
-
+    
+    Serial.println(" bytes read from Flash . Values are:");
+       for (int i = 0; i < EEPROM_SIZE; i++)
+        {
+           Serial.print(byte(EEPROM.read(i))); Serial.print(" ");
+        } 
     // Wait a bit before scanning again
     delay(5000);
+    for (i = 0; i < 8; i++)      TemperatureArray[i]++;
+      
+   if (loopCounter == 10) {
+
+      Serial.println("FLASH data written");
+      Serial.println(" bytes read from Flash . Values are:");
+    for (i = 0; i < 8; i++){ 
+     EEPROM.write(i, TemperatureArray[i]);
+    }
+    EEPROM.commit();
+      for (int i = 0; i < EEPROM_SIZE; i++)
+    {
+    Serial.print(byte(EEPROM.read(i))); Serial.print(" ");
+    }
+    }
 }
