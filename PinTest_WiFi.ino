00028c7676e9e9a19d85dce160942ef0996f9660
Pinelab <pinelab@seznam.cz>
2019-12-01 13:00:51 +0100

    parser implemented

--- PinTest_WiFi.ino
+++ PinTest_WiFi.ino
@@ -33,20 +33,66 @@ int RTC_DATA_ATTR LoopCount = 0;
 #define PUMP2  32 // backward compatibility
 #define COOLER 33 // backward compatibility
 
-#define EEPROM_SIZE 8
+#define EEPROM_SIZE 32
 #define uS_TO_S_FACTOR 1000000  /* Conversion factor for micro seconds to seconds */
 #define TIME_TO_SLEEP  5        /* Time ESP32 will go to sleep (in seconds) */
 
 signed char TemperatureArray[EEPROM_SIZE] = {0, 1, 2, 3, 4, 5, 6, 7}; // temporal temperature array
 signed char TimeArray[EEPROM_SIZE] = {0, 1, 2, 3, 4, 5, 6, 7}; // temporal temperature array
 
-signed char RTC_DATA_ATTR TemperatureArrayRTC[EEPROM_SIZE];
+//signed char RTC_DATA_ATTR TemperatureArrayRTC[EEPROM_SIZE];
+//signed char RTC_DATA_ATTR TimeArrayRTC[EEPROM_SIZE];
+
 signed char RTC_DATA_ATTR TimeArrayRTC[EEPROM_SIZE];
 struct RTC_DATA_ATTR tm timeinfo;
 
 int RTC_DATA_ATTR loopCounter = 0; // main loop counter
 
 int addr = 0;
+#define ARRAY_SIZE_PARMS 8
+int arrayParams[ARRAY_SIZE_PARMS];
+
+void parseParms(String strToParse) {
+  bool flag = false;
+  int arrayIndex = 0;
+  int numbIndex = 0;
+  int numb[4];
+
+  for (int i = 0; i < 26; i++) {
+    if (strToParse[i] == ':') {
+      if (flag) {
+        switch (numbIndex) {
+          case 1:
+            arrayParams[arrayIndex++] = numb[0];
+            break;
+          case 2:
+            arrayParams[arrayIndex++] = (numb[0]) * 10 + numb[1];
+            break;
+          case 3:
+            arrayParams[arrayIndex++] = (numb[0]) * 100 + (numb[1]) * 10 + numb[2];
+            break;
+          case 4:
+            arrayParams[arrayIndex++] = (numb[0]) * 100 + (numb[1]) * 100 + (numb[2]) * 10 + numb[3];
+            break;
+        }
+
+        if (arrayIndex == ARRAY_SIZE_PARMS) return;
+        for (int j = 0; j < 4 ; j++) {
+          numb[j] = 0;
+          numbIndex = 0;
+        }
+      }
+
+      flag = true;
+    }
+    else {
+      if (flag && ((int)strToParse[i] > 47)) {
+        numb[numbIndex] = (int)strToParse[i] - 48;
+        numbIndex++;
+      }
+    }
+  }
+}
 
 void printLocalTime()
 {
@@ -58,6 +104,7 @@ void printLocalTime()
 }
 
 void takeDataWeb() {
+  int i = 0;
   // Set WiFi to station mode and disconnect from an AP if it was previously connected
   //connect to WiFi
   Serial.printf("Connecting to %s ", ssid);
@@ -65,6 +112,8 @@ void takeDataWeb() {
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
+    i++;
+    if (i > 20) break;
   }
   Serial.println(" CONNECTED");
 
@@ -74,9 +123,9 @@ void takeDataWeb() {
   ///HTTP
   HTTPClient http;
   WiFiClient cli;
-  
+
   Serial.print("[HTTP] begin...\n");
-  http.begin(cli,httpSettings); //HTTP
+  http.begin(cli, httpSettings); //HTTP
   Serial.print("[HTTP] GET...\n");
   // start connection and send HTTP header
   int httpCode = http.GET();
@@ -91,6 +140,12 @@ void takeDataWeb() {
       String payload = http.getString();
       Serial.println("---------------------------------------");
       Serial.println(payload);
+      parseParms(payload);
+      Serial.println("---------------------------------------");
+      for (int i = 0; i < 6; i++) {
+        Serial.print(arrayParams[i]);
+        Serial.print(" : ");
+      }
       Serial.println("---------------------------------------");
     }
   } else {
@@ -108,32 +163,32 @@ void takeDataWeb() {
 void setup()
 {
   Serial.begin(115200);
-  
+
   pinMode (HEAT_RELAY, OUTPUT);
   digitalWrite(HEAT_RELAY, HIGH);
   pinMode (PUMP1, OUTPUT);
   digitalWrite(PUMP1, LOW);
   pinMode (PUMP2, OUTPUT);
-  digitalWrite(PUMP2, LOW); 
+  digitalWrite(PUMP2, LOW);
   pinMode (COOLER, OUTPUT);
-  digitalWrite(COOLER, LOW); 
-  
+  digitalWrite(COOLER, LOW);
+
   Serial.println(loopCounter);
   //if (LoopCount++ % 60 == 0) {
   //  takeDataWeb();
- // takeDataWeb();
-  
-    if (!EEPROM.begin(EEPROM_SIZE))
-    {
-      Serial.println("failed to initialise EEPROM"); delay(1000000);
-    }
-    Serial.println(" bytes read from Flash . Values are:");
-    for (int i = 0; i < EEPROM_SIZE; i++)
-    {
-      Serial.print(byte(EEPROM.read(i))); Serial.print(" ");
-    }
-    Serial.println("Setup done");
- // }
+  // takeDataWeb();
+
+  if (!EEPROM.begin(EEPROM_SIZE))
+  {
+    Serial.println("failed to initialise EEPROM"); delay(1000000);
+  }
+  Serial.println(" bytes read from Flash . Values are:");
+  for (int i = 0; i < EEPROM_SIZE; i++)
+  {
+    Serial.print(byte(EEPROM.read(i))); Serial.print(" ");
+  }
+  Serial.println("Setup done");
+  // }
   sensors.begin();
 }
 
@@ -141,40 +196,40 @@ void loop()
 {
   byte i;
   byte addr[8];
-  
+
   loopCounter++;
-  
+
   sensors.begin();
 
   Serial.println(" ");
   printLocalTime();
-  
+
   Serial.println(" ");
   if (loopCounter == 1)takeDataWeb();
-// if (LoopCount > 1){
+  // if (LoopCount > 1){
   sensors.requestTemperatures(); // Send the command to get temperatures
   Serial.print("Sensor 1(*C): ");
-  Serial.print(sensors.getTempC(sensor1)); 
+  Serial.print(sensors.getTempC(sensor1));
   Serial.print(" Sensor 2(*C): ");
-  Serial.print(sensors.getTempC(sensor2)); 
- //}
+  Serial.print(sensors.getTempC(sensor2));
+  //}
   //reload
- // esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);
+  // esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);
 
 
-  Serial.flush(); 
+  Serial.flush();
 
-  
+
   //if (loopCounter % 10 == 0){
- //  WiFi.mode(WIFI_STA);
- //  takeDataWeb();
+  //  WiFi.mode(WIFI_STA);
+  //  takeDataWeb();
   //  }
-    
-    delay(1000);
+
+  delay(1000);
   //digitalWrite(HEAT_RELAY, LOW);
   //delay(1000);
- //  esp_wifi_stop();
- // esp_light_sleep_start();
- // esp_deep_sleep_start();
+  //  esp_wifi_stop();
+  // esp_light_sleep_start();
+  // esp_deep_sleep_start();
   //Serial.println("This will never be printed");
 }
